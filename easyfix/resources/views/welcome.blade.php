fb4af977f4f57a20eebfd76af8c85390bc56f8be
Hussain Munad <munad@Munads-MacBook-Pro-2.local>
2026-02-05 21:04:31 +0500

    Test auto-deploy: Update page title

--- easyfix/resources/views/welcome.blade.php
+++ easyfix/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Easy Fix - Quick Home & Office Repairs in Malé</title>
+    <title>Easy Fix - Quick Home & Office Repairs in Greater Malé</title>
     <meta name="description" content="Fast, reliable repairs for your home and office in Malé City. AC, electrical, plumbing, cleaning and more. Our trained team is ready to help.">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
